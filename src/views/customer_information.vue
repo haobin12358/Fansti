eb87bf2a95afd6d1617c8556161683e8338239da
maoyaY <1335679005@qq.com>
2018-08-01 14:41:50 +0800

    input边框样式

--- src/views/customer_information.vue
+++ src/views/customer_information.vue
@@ -1,10 +1,27 @@
 <template>
     <div class="m-customer">
         <tabs :tabs="tabs_data" @tabClick="tabClick"></tabs>
-        <div>
-          123123
-        </div>
+        <div class="m-edit-content">
+          <el-form  :model="form" ref="form" :rules="rules" label-width="1.2rem" class="demo-ruleForm" >
+              <el-form-item label="客服名称：" prop="name">
+                <el-input v-model="form.name" class="m-input-l" placeholder="最多10个字" maxlength="10"></el-input>
+              </el-form-item>
 
+            <el-form-item label="客服电话：" prop="qq">
+              <el-input v-model="form.telphone" class="m-input-l" placeholder="最多10个字" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="客服qq：" prop="qq">
+              <el-input v-model="form.qq" class="m-input-l" placeholder="最多10个字" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="客服邮箱：" prop="email">
+              <el-input v-model="form.email" class="m-input-l" placeholder="最多10个字" ></el-input>
+            </el-form-item>
+
+              <div class="m-bottom-btn m-flex-center">
+               <span>删除</span>
+              </div>
+          </el-form>
+        </div>
     </div>
 </template>
 
@@ -15,16 +32,31 @@
           return {
             tabs_data:[
               {
-                name:'上传',
+                name:'编辑',
                 click:true,
                 url:''
-              },
-              {
-                name:'编辑',
-                click:false,
-                url:''
               }
-            ]
+            ],
+            form:{
+              name:'',
+              qq:'',
+              telphone:'',
+              email:''
+            },
+            rules:{
+              name:[
+                { required: true, message: '请输入客服名称', trigger: 'blur' }
+              ],
+              qq:[
+                {required:true,message:'请输入客户qq',trigger:'blur'}
+              ],
+              telphone:[
+                {required:true,message:'请输入客户电话',trigger:'blur'}
+              ],
+              email:[
+                {required:true,message:'请输入客户邮箱',trigger:'blur'}
+              ]
+            }
           }
       },
       components:{
