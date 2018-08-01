ef60b3dca75d4b7ddae59024aae3fea7c2a3703d
maoyaY <1335679005@qq.com>
2018-08-01 10:58:51 +0800

    登录样式修改

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -151,7 +151,7 @@
     width: 100%;
     height: 100%;
     .login-box{
-      width: 4rem;
+      width: 3.6rem;
        height: 4.6rem;
       padding: 0.6rem;
       position: absolute;
@@ -201,7 +201,7 @@
           background-size: 100% 100%;
         }
         button{
-          width: 4rem;
+          width: 3.6rem;
           height: 0.54rem;
           text-align: center;
           font-size: 0.2rem;
