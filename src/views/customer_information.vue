0db98e69248a9b4a08d63fb916c975d77990d3e7
maoyaY <1335679005@qq.com>
2018-08-01 15:24:13 +0800

    客服信息编辑页面

--- src/views/customer_information.vue
+++ src/views/customer_information.vue
@@ -1,42 +1,50 @@
 <template>
     <div class="m-customer">
-        <tabs :tabs="tabs_data" @tabClick="tabClick"></tabs>
+        <p class="m-title">客服信息编辑</p>
         <div class="m-edit-content">
-          <el-form  :model="form" ref="form" :rules="rules" label-width="1.2rem" class="demo-ruleForm" >
-              <el-form-item label="客服名称：" prop="name">
-                <el-input v-model="form.name" class="m-input-l" placeholder="最多10个字" maxlength="10"></el-input>
-              </el-form-item>
-
-            <el-form-item label="客服电话：" prop="qq">
-              <el-input v-model="form.telphone" class="m-input-l" placeholder="最多10个字" type="number"></el-input>
-            </el-form-item>
-            <el-form-item label="客服qq：" prop="qq">
-              <el-input v-model="form.qq" class="m-input-l" placeholder="最多10个字" type="number"></el-input>
-            </el-form-item>
-            <el-form-item label="客服邮箱：" prop="email">
-              <el-input v-model="form.email" class="m-input-l" placeholder="最多10个字" ></el-input>
-            </el-form-item>
-
-              <div class="m-bottom-btn m-flex-center">
-               <span>删除</span>
-              </div>
-          </el-form>
+          <ul class="m-card">
+            <li @click="show_modal">
+              <img src="" alt="" class="m-img">
+              <p>姓名</p>
+              <p>454512454545</p>
+              <!--<span class="m-close"></span>-->
+            </li>
+          </ul>
         </div>
+      <el-dialog
+        title="编辑"
+        :visible.sync="showModal"
+        width="50%"
+        center>
+        <el-form  :model="form" ref="form" :rules="rules" label-width="2rem" class="demo-ruleForm" >
+          <el-form-item label="客服名称：" prop="name">
+            <el-input v-model="form.name" class="m-input-l" placeholder="此处为客服名称" maxlength="10"></el-input>
+          </el-form-item>
+
+          <el-form-item label="客服电话：" prop="qq">
+            <el-input v-model="form.telphone" class="m-input-l" placeholder="此处为客服电话" type="number"></el-input>
+          </el-form-item>
+          <el-form-item label="客服qq：" prop="qq">
+            <el-input v-model="form.qq" class="m-input-l" placeholder="此处为客服qq" type="number"></el-input>
+          </el-form-item>
+          <el-form-item label="客服邮箱：" prop="email">
+            <el-input v-model="form.email" class="m-input-l" placeholder="此处为客服邮箱" ></el-input>
+          </el-form-item>
+
+          <div class="m-bottom-btn m-flex-center">
+            <span class="m-btn active">修改</span>
+          </div>
+        </el-form>
+      </el-dialog>
     </div>
 </template>
 
 <script>
-  import tabs from '../components/common/tabs';
+    import api from '../api/api';
+    import axios from 'axios';
     export default {
       data(){
           return {
-            tabs_data:[
-              {
-                name:'编辑',
-                click:true,
-                url:''
-              }
-            ],
             form:{
               name:'',
               qq:'',
@@ -56,25 +64,86 @@
               email:[
                 {required:true,message:'请输入客户邮箱',trigger:'blur'}
               ]
-            }
+            },
+            showModal:false,
+            customer_data:[
+              {
+
+              }
+            ]
           }
-      },
-      components:{
-        tabs
       },
       methods:{
-        tabClick(index){
-          let _arr = this.tabs_data;
-          for(let i =0;i<_arr.length;i++){
-            _arr[i].click = false;
-          }
-          _arr[index].click = true;
-          this.tabs_data = [].concat(_arr);
+        show_modal(){
+          this.showModal = true;
         }
       }
     }
 </script>
 
 <style lang="less" rel="stylesheet/less" scoped>
-
+  @import "../common/css/_variate";
+.m-input-l{
+  width: 6rem;
+}
+  .m-customer{
+    .m-title{
+      font-size: 0.18rem;
+      margin-bottom: 0.3rem;
+    }
+    ul.m-card{
+      display: flex;
+      flex-flow: row;
+      align-content: center;
+      flex-wrap: wrap;
+      li{
+        position: relative;
+        width: 2.6rem;
+        padding: 0.2rem;
+        height: 3.6rem;
+        border: 1px solid @borderColor;
+        margin-right: 0.6rem;
+        margin-bottom: 0.3rem;
+        cursor: pointer;
+        .m-img{
+          display: block;
+          width: 2.6rem;
+          height: 2.6rem;
+          border: 1px solid @borderColor;
+          margin-bottom: 0.2rem;
+        }
+        p{
+          font-size: 0.14rem;
+          text-align: center;
+          line-height: 0.34rem;
+        }
+        .m-close{
+          position: absolute;
+          top:0;
+          right: -0.4rem;
+          width: 0.3rem;
+          height: 0.3rem;
+          background: url("../common/images/icon-close.png");
+          background-size: 100%;
+        }
+      }
+    }
+  }
+  .m-bottom-btn{
+    margin: 0.5rem;
+    .m-btn{
+      display: inline-block;
+      width: 0.8rem;
+      height: 0.4rem;
+      line-height: 0.4rem;
+      text-align: center;
+      background-color: @btnColor;
+      margin-right: 0.4rem;
+      cursor: pointer;
+      color: #fff;
+      &.active{
+        background-color: @btnActiveColor;
+      }
+    }
+  }
 </style>
