d8813f3b30017f12b6eca2f2da306bf473c08044
ygd <helloworld8881@sina.com>
2019-04-30 00:23:48 +0800

    feat(到电子交接单): 调接口,剩文件和提交

--- src/api/api.js
+++ src/api/api.js
@@ -1,77 +1,89 @@
 import {
   wxRequest
 } from '@/utils/wxRequest';
+
 const mock = false;
 
-const host =mock? 'https://dsn.apizza.net/mock/60c954072cfff536376e5acb0392c590' :'https://fstwechat.com/fansti';
+const host = mock ? 'https://dsn.apizza.net/mock/60c954072cfff536376e5acb0392c590' : 'https://fstwechat.com/fansti';
+
+// 计划
+const getTodayList = params => wxRequest(params, host + `/control/get_today_list`);
+const getCJcAbo = params => wxRequest(params, host + `/control/get_jc_abo`);
+const getHandoverList = params => wxRequest(params, host + `/control/get_handover_list`);
+const getJcPic = params => wxRequest(params, host + `/control/get_jc_pic`);
+const getJcCb = params => wxRequest(params, host + `/control/get_jc_cb`);
+const getFkdw = params => wxRequest(params, host + `/control/get_fkdw`);
+const getFyzl = params => wxRequest(params, host + `/control/get_fyzl`);
+const getInAbo = params => wxRequest(params, host + `/control/get_in_abo`);
+const getOutAbo = params => wxRequest(params, host + `/control/get_out_abo`);
+const getHcAbo = params => wxRequest(params, host + `/control/get_hc_abo`);
+const getSbList = params => wxRequest(params, host + `/control/get_sb_list`);
+const getBzsmList = params => wxRequest(params, host + `/control/get_bzsm_list`);
+const getCJdList = params => wxRequest(params, host + `/control/get_jd_list`);
+const getSbnoList = params => wxRequest(params, host + `/control/get_sbno_list`);
+const updateWts = params => wxRequest(params, host + `/control/update_wts`); // post
+
 
 //  绑定用户  POST
-const userBinding = params => wxRequest(params, host+'/users/user_binding');
+const userBinding = params => wxRequest(params, host + '/users/user_binding');
 //  查看绑定状态
-const getBinding = params => wxRequest(params, host+'/users/get_binding');
+const getBinding = params => wxRequest(params, host + '/users/get_binding');
 //  获取微信id  POST
-const getOpenid = params => wxRequest(params, host+'/users/get_openid');
+const getOpenid = params => wxRequest(params, host + '/users/get_openid');
 
 //  获取新闻列表  POST
-const getAllNews = params => wxRequest(params, host+`/news/get_all?page_size=${params.page_size}&page_num=${params.page_num}`);
+const getAllNews = params => wxRequest(params, host + `/news/get_all?page_size=${params.page_size}&page_num=${params.page_num}`);
 //  获取新闻列表  POST
-const getNewsAbo = params => wxRequest(params, host+`/news/get_abo?id=${params.id}`);
+const getNewsAbo = params => wxRequest(params, host + `/news/get_abo?id=${params.id}`);
 
 //  获取支付数据
-const payService = params => wxRequest(params, host+`/pay/pay_service`);
-
-
+const payService = params => wxRequest(params, host + `/pay/pay_service`);
 
 
 //  获取货物列表
-const getGoodsList = params => wxRequest(params, host+`/goods/get_goods_list`);
+const getGoodsList = params => wxRequest(params, host + `/goods/get_goods_list`);
 //  获取货物详情
-const getJcAbo = params => wxRequest(params, host+'/goods/get_jc_abo');
+const getJcAbo = params => wxRequest(params, host + '/goods/get_jc_abo');
 //  确认照片  POST
-const retrueGoods = params => wxRequest(params, host+`/goods/retrue_goods?login_name=${params.login_name}&jcno=${params.jcno}`);
+const retrueGoods = params => wxRequest(params, host + `/goods/retrue_goods?login_name=${params.login_name}&jcno=${params.jcno}`);
 
 //  查询dgr
-const getDgr = params => wxRequest(params, host+'/scrapy/get_dgr');
+const getDgr = params => wxRequest(params, host + '/scrapy/get_dgr');
 //  查询鉴定信息
-const getJd = params => wxRequest(params, host+'/scrapy/get_jd');
+const getJd = params => wxRequest(params, host + '/scrapy/get_jd');
 //  模糊查询鉴定信息
-const getJdList = params => wxRequest(params, host+'/scrapy/get_jd_names');
+const getJdList = params => wxRequest(params, host + '/scrapy/get_jd_names');
 //  查询tact
-const getTact = params => wxRequest(params, host+'/scrapy/get_tact');
+const getTact = params => wxRequest(params, host + '/scrapy/get_tact');
 //  查询hscode 爬虫
-const getHs = params => wxRequest(params, host+'/scrapy/get_hs');
+const getHs = params => wxRequest(params, host + '/scrapy/get_hs');
 //  查询cas 爬虫
-const getCas = params => wxRequest(params, host+'/scrapy/get_cas');
+const getCas = params => wxRequest(params, host + '/scrapy/get_cas');
 //  查询航班信息
-const getFlyno = params => wxRequest(params, host+'/scrapy/get_flyno');
+const getFlyno = params => wxRequest(params, host + '/scrapy/get_flyno');
 
 
 //  获取红包全部内容
-const getAllRed = params => wxRequest(params, host+'/reds/get_all_red');
-const receiveRed = params => wxRequest(params, host+'/reds/receive_red');
-const receiveRedQuery = params => wxRequest(params, host+'/reds/receive_red_query');
+const getAllRed = params => wxRequest(params, host + '/reds/get_all_red');
+const receiveRed = params => wxRequest(params, host + '/reds/receive_red');
+const receiveRedQuery = params => wxRequest(params, host + '/reds/receive_red_query');
 //  留言
-const makeUserMessage = params => wxRequest(params, host+`/users/make_user_message?openid=${params.openid}`);
+const makeUserMessage = params => wxRequest(params, host + `/users/make_user_message?openid=${params.openid}`);
 //  获取客服信息
-const getCustom = params => wxRequest(params, host+`/other/get_custom?login_name=${params.login_name}`);
+const getCustom = params => wxRequest(params, host + `/other/get_custom?login_name=${params.login_name}`);
 //  获取个人名片
-const getMyInfo = params => wxRequest(params, host+`/users/get_my_info?openid=${params.openid}`);
+const getMyInfo = params => wxRequest(params, host + `/users/get_my_info?openid=${params.openid}`);
 //  更新个人名片
-const updateMyInfo = params => wxRequest(params, host+`/users/update_my_info?openid=${params.openid}`);
+const updateMyInfo = params => wxRequest(params, host + `/users/update_my_info?openid=${params.openid}`);
 //  邀请用户
-const addInvate = params => wxRequest(params, host+`/users/add_invate?openid=${params.openid}`);
+const addInvate = params => wxRequest(params, host + `/users/add_invate?openid=${params.openid}`);
 //  获取邀请人列表
-const getInvateList = params => wxRequest(params, host+`/users/get_invate_list`);
+const getInvateList = params => wxRequest(params, host + `/users/get_invate_list`);
 
 //  查询问卷
-const getVote = params => wxRequest(params, host+`/votes/get_vote?VSid=${params.VSid}&VOno=${params.VOno}`);
+const getVote = params => wxRequest(params, host + `/votes/get_vote?VSid=${params.VSid}&VOno=${params.VOno}`);
 //  做问卷 POST
-const makeVote = params => wxRequest(params, host+`/votes/make_vote`);
-
-const getTodayList = params => wxRequest(params, host+`/control/get_today_list`);
-
-
-
+const makeVote = params => wxRequest(params, host + `/votes/make_vote`);
 
 
 export default {
@@ -103,4 +115,18 @@ export default {
   receiveRedQuery,
   payService,
   getTodayList,
-}
+  getCJcAbo,
+  getHandoverList,
+  getJcPic,
+  getJcCb,
+  getFkdw,
+  getFyzl,
+  getInAbo,
+  getOutAbo,
+  getHcAbo,
+  getSbList,
+  getBzsmList,
+  getCJdList,
+  getSbnoList,
+  updateWts,
+};
