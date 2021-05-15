20b1dbd043934970a05b39ca7f162ea8fefd2cfc
杨国栋 <helloworld8881@sina.com>
2021-05-15 23:26:44 +0800

    nojc search

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,29 @@
-export const Origins = [{
-  label: '上海',
-  value: 1
-}];
+export const flightOptions = [
+  {
+    label: '全部',
+    value: ''
+  }, {
+    label: 'CA',
+    value: 'CA'
+  }, {
+    label: 'BGS',
+    value: 'BGS'
+  }
+];
+
+export const originOptions = [
+  {
+    label: '全部区域',
+    value: ''
+  }, {
+    label: '北京',
+    value: '0'
+  }, {
+    label: '上海',
+    value: '1'
+  }, {
+    label: '广州',
+    value: '2'
+  }
+];
+
